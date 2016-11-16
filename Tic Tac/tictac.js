5c2eaf95058d02fe1cec4b659fba2f2bccfa0f9a
Varudhini Ruttala <c_vruttala@innominds.com>
2016-11-16 19:38:54 +0530

    tic tac update

--- Tic Tac/tictac.js
+++ Tic Tac/tictac.js
@@ -1,27 +1,30 @@
 $(document).ready(function(){
-  $(".active_block").click(function(activeTarget){
-      var ticPlayer=activeTarget.target;
 
+
+
+function playerToggle(){
       var ticplayer1 = $(".player1").hasClass("active_player");
       var ticplayer2 = $(".player2").hasClass("active_player");
 
 
-      if(ticplayer1 && !ticplayer2){
+      if(ticplayer1){
 
-       $(".player1").removeClass(".active_player");
-       $(".player2").addClass(".active_player");
-       $("ticPlayer").css("background-color:purple");
+       $(".player1").removeClass("active_player");
+       $(".player2").addClass("active_player");
 
 
       }
-      if(!ticplayer1 && ticplayer2){
-        $(".player1").addClass(".active_player");
-        $(".player2").removeClass(".active_player");
-        $("ticPlayer").css("background-color:orange");
+      if(ticplayer2){
+        $(".player1").addClass("active_player");
+        $(".player2").removeClass("active_player");
+
       }
+    
+}
 
-  });
-
-
+$(".header1").click(playerToggle);
+$(".header2").click(playerToggle);
+$(".header3").click(playerToggle);
+$(".header4").click(playerToggle);
 
 });
