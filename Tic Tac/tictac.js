940248abc3a2d714e64ddf33859df6cfe8c4b478
Varudhini Ruttala <c_vruttala@innominds.com>
2016-11-17 12:22:14 +0530

    Tic TIC GAME

--- Tic Tac/tictac.js
+++ Tic Tac/tictac.js
@@ -5,26 +5,31 @@ $(document).ready(function(){
 function playerToggle(){
       var ticplayer1 = $(".player1").hasClass("active_player");
       var ticplayer2 = $(".player2").hasClass("active_player");
-
+      var id = this.id;
 
       if(ticplayer1){
 
        $(".player1").removeClass("active_player");
        $(".player2").addClass("active_player");
+       $("#"+id).text("*");
+        $("#"+id).addClass("active_div");
+
 
 
       }
       if(ticplayer2){
         $(".player1").addClass("active_player");
         $(".player2").removeClass("active_player");
+        $("#"+id).text("#");
+        $("#"+id).addClass("active_div2");
 
       }
-    
+
 }
 
-$(".header1").click(playerToggle);
-$(".header2").click(playerToggle);
-$(".header3").click(playerToggle);
-$(".header4").click(playerToggle);
+    $(".header1").one("click",playerToggle);
+    $(".header2").one("click",playerToggle);
+    $(".header3").one("click",playerToggle);
+    $(".header4").one("click",playerToggle);
 
 });
