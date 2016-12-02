85f975c8b739ba40924d1ab587d422b27559f5cb
VarudhiniRao <varudhinirao26@gmail.com>
2016-12-02 18:28:35 +0530

    Add files via upload

--- Table-less/js/getData.js
+++ Table-less/js/getData.js
@@ -1,32 +1,58 @@
 var app = angular.module('myApp', []);
 
-app.service('dataService', function($http) {
-    this.getData = function(callbackFunc) {
-        $http.get('js/document.json').success(function(data) {
-            console.log(data)
-            callbackFunc(data);
+// app.service('dataService', function($http) {
+//     this.getData = function(callbackFunc) {
+//         $http.get('js/document.json').success(function(data) {
+//             console.log(data)
+//             callbackFunc(data);
 
-        }).error(function() {
-            console.log("error");
-        });
-    }
-});
+//         }).error(function() {
+//             console.log("error");
+//         });
+//     }
+// });
 
-app.controller('getDataCtrl', function($scope, dataService) {
-    //$scope.click = function() {
-    $scope.myData = {};
-    dataService.getData(function(dataResponse) {
-        $scope.myData = dataResponse.AlphaNumeric;
-        console.log($scope.myData)
-    });
+// app.controller('getDataCtrl', function($scope, dataService) {
+//     //$scope.click = function() {
+//     $scope.myData = {};
+//     dataService.getData(function(dataResponse) {
+//         $scope.myData = dataResponse.AlphaNumeric;
+//         console.log($scope.myData)
+//     });
 
-    //};
-
-
+//     //};
 
 
-});
 
+
+// });
+	app.service('promiseService',function($http,$q){
+	 
+		 this.getjson=function(url){
+
+		  var getData=$q.defer();
+		 
+		  $http.get(url).success(getData.resolve)
+		  
+		  .error(getData.reject);
+		  
+		  return getData.promise;
+		   
+	  	console.log(getData.promise); 
+	 }
+	
+	});
+		 
+		 app.controller('getDataCtrl',function($scope,promiseService){
+		 // alert('hi');
+			 
+			  promiseService.getjson('js/document.json').then(function(dataResponse){
+			   $scope.myData=dataResponse.AlphaNumeric;
+			   console.log($scope.myData);
+			 
+			  });
+		
+		 });
 
 
 app.directive('jsonTemplate', function() {
